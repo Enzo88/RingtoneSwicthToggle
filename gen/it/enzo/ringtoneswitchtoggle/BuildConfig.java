f08121a539ce1ba0963333acea42540a7b379d50
Vincenzo Pimpinella <vincenzo.pimpinella@autosoft.it>
2013-07-17 15:18:09 +0200

    Initial Commit

---
+++ gen/it/enzo/ringtoneswitchtoggle/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package it.enzo.ringtoneswitchtoggle;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/it/enzo/ringtoneswitchtoggle/SwitchToggle.java
@@ -0,0 +1,7 @@
+package it.enzo.ringtoneswitchtoggle;
+
+import android.appwidget.AppWidgetProvider;
+
+public class SwitchToggle extends AppWidgetProvider {
+	
+}
